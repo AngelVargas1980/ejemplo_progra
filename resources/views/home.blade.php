d8ee16df269bbccb6a7b77a0faf20db568ee9f58
Ángel Antonio Vargas Lázaro <angel.vargas@intecap.edu.gt>
2023-02-25 15:17:08 -0600

    tercer tercer commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,9 @@
 
 @section('content')
     <p>Este es el contenido de la vista</p>
+<<<<<<< HEAD
     <a href="{{route('pagina2 pru2')}}"> Links a pagina 2</a>
+=======
+    <a href="{{route('pagina2 pru')}}"> Links a pagina 2</a>
+>>>>>>> 80c7d65 (probando primer commit)
 @endsection
